83cc5c79d148458c9a7ba8937458bf70fe21c40c
EnockYator <ekyator02@gmail.com>
2024-02-19 12:30:18 +0300

    set react frontend server

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Doctor Appointment Booking</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
