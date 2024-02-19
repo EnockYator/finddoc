ee68245ff6f9bbb2c4c0f117c65dcf9d97f53dac
EnockYator <ekyator02@gmail.com>
2024-02-19 12:31:46 +0300

    set up backend server and database

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// import express module
+const express = require("express");
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// import the mongoose module
+const mongoose = require('mongoose');
+
+//connect to mongodb using connection url stored in
+//the environment variable MONGO_URL
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("MongoDB connected successfully!");
+})
+.catch((err) => {
+    console.error("error connecting to MongoDB", err.message);
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
+// testing whether the environment variables works
+console.log(process.env.MONGO_URL);
