ea5b970364715cb5a673f8bfffe7aba18fa1ab3d
EnockYator <ekyator02@gmail.com>
2024-02-23 16:09:48 +0300

    route pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
+import Appointments from './components/Appointments';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/appointments" element={<Appointments />}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
