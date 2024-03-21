0496735732f0837551278c4940e17a7850f4c952
EnockYator <ekyator02@gmail.com>
2024-03-21 17:22:26 +0300

    import and route more components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import Appointments from './components/Appointments';
+import PrivacyPolicy from './components/Privacy-policy';
+import TermsOfService from './components/Terms-of-service';
+import ContactDeveloper from './components/Contact-developer';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/appointments" element={<Appointments />}/>
+        <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
+        <Route path="/terms-of-service" element={<TermsOfService />}/>
+        <Route path="/contact-developer" element={<ContactDeveloper />}/>
 
       </Routes>
     </BrowserRouter>
